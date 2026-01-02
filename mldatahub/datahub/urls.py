deaf32f734f4c93704fc369d71fbda1de5940398
Aleksander <251544@edu.p.lodz.pl>
2026-01-02 18:01:30 +0100

    views, templates, error pages

--- mldatahub/datahub/urls.py
+++ mldatahub/datahub/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "datahub"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("delete/<int:record_id>/", views.delete, name="delete"),
+    path("add", views.add, name="add"),
+    path("api/data", views.api_data, name="api_data"),
+    path("api/add", views.api_add, name="api_add"),
+    # path("api/delete", views.api_delete, name="api_delete"),
 ]
--- mldatahub/datahub/views.py
+++ mldatahub/datahub/views.py
@@ -1,7 +1,95 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+from http import HTTPStatus
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, Http404, JsonResponse, HttpResponseBadRequest
 from .models import Record
 
 
 def index(request):
-    return HttpResponse(Record.objects.all())
+    records = Record.objects.order_by("id")
+    context = {"records": records}
+    return render(request, "datahub/index.html", context)
+
+
+def delete(request, record_id):
+    if request.method == "POST":
+        try:
+            record = get_object_or_404(Record, pk=record_id)
+            record.delete()
+            return redirect("datahub:index")
+        except Http404:
+            return render(request, "datahub/error_404.html", status=404)
+    else:
+        return render(request, "datahub/error_400.html", status=400)
+
+
+def add(request):
+    if request.method == "POST":
+        try:
+            continuous_feature1 = float(request.POST.get("float1"))
+            continuous_feature2 = float(request.POST.get("float2"))
+            categorical_feature1 = int(request.POST.get("int_value"))
+
+            Record.objects.create(
+                continuous_feature1=continuous_feature1,
+                continuous_feature2=continuous_feature2,
+                categorical_feature1=categorical_feature1
+            )
+
+            return redirect("datahub:index")
+        except (ValueError, TypeError):
+            return render(request, "datahub/error_400.html", status=400)
+    else:
+        # render form
+        return render(request, "datahub/add.html", status=200)
+
+
+def api_data(request):
+    records = Record.objects.all()
+    data = []
+    for record in records:
+        data.append({
+            "id": record.id,
+            "continuous_feature1": record.continuous_feature1,
+            "continuous_feature2": record.continuous_feature2,
+            "categorical_feature1": record.categorical_feature1
+        })
+    return JsonResponse(data, safe=False)
+
+
+def api_add(request):
+    if request.method == "POST":
+        try:
+            body = json.load(request.body)
+
+            record = Record.objects.create(
+                continuous_feature1=body["continuous_feature1"],
+                continuous_feature2=body["continuous_feature2"],
+                categorical_feature1=body["categorical_feature1"]
+            )
+            return JsonResponse({"record_id": record.id}, status=HTTPStatus.CREATED)
+        except (ValueError, TypeError, KeyError) as e:
+            return HttpResponseBadRequest(
+                {
+                    "error": e,
+                    "message": "Invalid data",
+                }
+            )
+    else:
+        return HttpResponseBadRequest(
+            {
+                "message": "Invalid method",
+            }
+        )
+
+
+# DELETE /api/data/<record_id>
+# (where <record_id> stands for the primary key of a record) -
+# deletes a data point from the database.
+#  The <record_id> should be validated to check if the database contains a record with a matching primary key
+# and if the validation succeeds,
+#   the corresponding record should be deleted from the database
+#   and a response comprising a JSON that contains a dictionary specifying the primary key of the deleted record should follow.
+# If the validation fails,
+#   a response incorporating the 404 HTTP status code and comprising a JSON that contains a dictionary specifying
+#   a relevant error message (e.g., "Record not found") should be generated.
